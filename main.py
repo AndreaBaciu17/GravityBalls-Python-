840857b5f852f2285298441335212e7ef0bf06f7
Andrea B. <baciuandrea04@gmail.com>
2024-10-01 14:10:12 -0400

    Intro message added

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 import pygame
 
 # Screen dimensions
-width = 700
+width = 1000
 height = 700
 background_color = (65, 23, 62)
 
@@ -23,32 +23,79 @@ class GravityBall:
     def movement(self):
          self.rect = self.rect.move(self.speed)   
 
+# function for the intro screen before game begins
+def intro_screen(screen):
+     # Font of text
+     intro_text_color = (233, 114, 223)  # Pink color for text
+     hover_text_color = (255, 255, 255)  # White color for hover effect for the button
+     heading_font = pygame.font.Font(None, 100) #Title size
+     subheading_font = pygame.font.Font(None, 70) #description size
+     button_font = pygame.font.Font(None, 60) #button size
+     
+     # Text input
+     text_surface = heading_font.render("Welcome to Gravity Balls", True, intro_text_color)
+     subtext_surface = subheading_font.render("Ready to play?", True, intro_text_color)
+     button_surface = button_font.render("Start Game", True, intro_text_color)
+
+     button_rect = button_surface.get_rect(center = (width // 2, height // 2 + 90)) #Centers button
+
+    # to recognize when user clicks screen
+     while True:
+         for event in pygame.event.get():
+             if event.type == pygame.QUIT:
+                 pygame.quit()
+                 return
+             if event.type == pygame.MOUSEBUTTONDOWN:
+                 #checks if button clicked
+                 if button_rect.collidepoint(event.pos):
+                    return
+                    
+         # Creates Display for background
+         screen.fill(background_color)
+         # Creates pink to white animation when user hovers over button
+         cursor_pos = pygame.mouse.get_pos()
+         if button_rect.collidepoint(cursor_pos):
+             button_surface = button_font.render("START GAME", True, hover_text_color)
+         else:
+             button_surface = button_font.render("START GAME", True, intro_text_color)
+         # Creates Display for text and button
+         screen.blit(text_surface, (width // 2 - text_surface.get_width() // 2, height // 2 - 150))
+         screen.blit(subtext_surface, (width // 2 - subtext_surface.get_width() // 2, height // 2 - 50))
+         screen.blit(button_surface, button_rect)
+         pygame.display.flip()
+
+
 def main():
-        pygame.init() #initiates game library
-        screen = pygame.display.set_mode((width,height)) #sets image of display to output
-        fps = pygame.time.Clock() #creates timer when frame animation occurs
-        balls = []
-        while True:
-            for event in pygame.event.get():
-                  if event.type == pygame.QUIT: # 'QUIT' function to stop the game if user exits out
-                       pygame.quit()
-                       return
-                  # checks for cursor clicking events, and when user clicks, creates new ball
-                  if event.type == pygame.MOUSEBUTTONDOWN:
-                       x, y = event.pos # postion where user clicks
-                       new_ball = GravityBall(x, y) # creates new ball at position user clicks
-                       balls.append(new_ball) #creates new ball result after user input
-            screen.fill(background_color) # clear screen  
+    pygame.init() #initiates game library
+    screen = pygame.display.set_mode((width,height)) #sets image of display to output
+    fps = pygame.time.Clock() #creates timer when frame animation occurs
+    balls = []
+    
+    # initiates intro screen before game starts
+    intro_screen(screen)
 
-            # update and draws multiple balls
-            for ball in balls:
-                ball.update()
-                screen.blit(ball.image, ball.rect) #command to display ball into set background
-                #add movement and speed aspects
-            pygame.display.flip() #updating screen
-            fps.tick(120) #120 fps (frames per seconds)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # 'QUIT' function to stop the game if user exits out
+                pygame.quit()
+                return
+            # checks for cursor clicking events, and when user clicks, creates new ball
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos # postion where user clicks
+                new_ball = GravityBall(x, y) # creates new ball at position user clicks
+                balls.append(new_ball) #creates new ball result after user input
+        screen.fill(background_color) # clear screen  
 
-if __name__=="__main__": #the main program that runs
+        # update and draws multiple balls
+        for ball in balls:
+            ball.update()
+            screen.blit(ball.image, ball.rect) #command to display ball into set background
+
+        #add movement and speed aspects (ouside of for loop above to prevent flickering)
+        pygame.display.flip() #updating screen
+        fps.tick(120) #120 fps (frames per seconds)
+
+if __name__=="__main__": # initiates the main program to run
     main()
 
 
